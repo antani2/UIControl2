a6762e73c7919462f23813a089c2b8261c128270
antani2 <antti.tani@airbus.com>
2017-09-11 22:07:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/l5581/uicontrol2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.l5581.uicontrol2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // AutoCompleteTextView
+        AutoCompleteTextView actv = (AutoCompleteTextView)
+                findViewById(R.id.login); // add stings to control
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                new String[]
+                        {"Pasi","Juha","Kari","Jouni","Esa","Hannu"});
+        actv.setAdapter(aa);
+    }
+}
